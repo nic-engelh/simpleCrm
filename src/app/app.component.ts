c487c0662d71e898adcffc647df315ec8daec095
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-08-06 16:19:38 +0200

    Added dialog component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatSidenavModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatSidenavModule, MatButtonModule, MatIconModule, RouterLink, MatDialogModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {
+  MatDialog,
+} from '@angular/material/dialog';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
+
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, MatTooltipModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+
+  constructor(public dialog: MatDialog) {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+  }
+
+  openDialog() {
+    this.dialog.open(DialogAddUserComponent);
+  }
 
 }
