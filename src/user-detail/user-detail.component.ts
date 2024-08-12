3791cd32373c73499d9c783f98356cacf0bac923
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-08-12 22:22:14 +0200

    Added router id reader method to user details

--- src/user-detail/user-detail.component.ts
+++ src/user-detail/user-detail.component.ts
@@ -2,6 +2,9 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import { UserComponent } from '../app/user/user.component';
+import { ActivatedRoute } from '@angular/router';
+import { doc, getDoc } from '@angular/fire/firestore';
+import { Firestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-user-detail',
@@ -11,5 +14,24 @@ import { UserComponent } from '../app/user/user.component';
   styleUrl: './user-detail.component.scss'
 })
 export class UserDetailComponent {
+  id: string | null = null;
+  document: any;
 
-}
+  constructor(private route: ActivatedRoute, private afs: Firestore) {}
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id');
+    if (this.id) {
+      const docRef = doc(this.afs, `users/${this.id}`);
+      getDoc(docRef).then((docSnap) => {
+        if (docSnap.exists()) {
+          this.document = docSnap.data();
+          console.log("Document:", this.document)
+        } else {
+          console.log("No such document!");
+        }
+      });
+    }
+  }
+  }
+
