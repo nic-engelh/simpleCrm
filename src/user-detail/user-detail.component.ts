c238569ccc4da89be5142046971509b58f9e3b81
Niklas <110847147+nic-engelh@users.noreply.github.com>
2024-08-14 15:51:33 +0200

    Added new components and user detail getdoc features

--- src/user-detail/user-detail.component.ts
+++ src/user-detail/user-detail.component.ts
@@ -1,12 +1,16 @@
+import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { UserComponent } from '../app/user/user.component';
 import { ActivatedRoute } from '@angular/router';
 import { doc, getDoc } from '@angular/fire/firestore';
 import { Firestore } from '@angular/fire/firestore';
 import { Subscription } from 'rxjs';
+import { User } from '../models/user.class';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'app-user-detail',
@@ -16,6 +20,10 @@ import { Subscription } from 'rxjs';
     MatCardModule,
     UserComponent,
     MatProgressSpinnerModule,
+    UserComponent,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
   ],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss',
@@ -23,9 +31,10 @@ import { Subscription } from 'rxjs';
 export class UserDetailComponent implements OnInit, OnDestroy {
   id: string | null = null;
   document: any;
+  user: User | null = null;
   private subscription: Subscription | null = null;
 
-  constructor(private route: ActivatedRoute, private afs: Firestore) {}
+  constructor(private route: ActivatedRoute, private firestore: Firestore) {}
 
   ngOnInit(): void {
     this.subscription = this.route.paramMap.subscribe((params) => {
@@ -33,13 +42,13 @@ export class UserDetailComponent implements OnInit, OnDestroy {
     });
 
     if (this.id) {
-      const docRef = doc(this.afs, `users/${this.id}`);
+      const docRef = doc(this.firestore, `users/${this.id}`);
       getDoc(docRef).then((docSnap) => {
         if (docSnap.exists()) {
-          this.document = docSnap.data();
-          console.log('Document:', this.document);
+          this.user = docSnap.data() as User;
+          console.log('User:', this.user);
         } else {
-          console.log('No such document!');
+          console.log('No such user!');
         }
       });
     }
@@ -49,5 +58,9 @@ export class UserDetailComponent implements OnInit, OnDestroy {
     if (this.subscription) {
       this.subscription.unsubscribe();
     }
+  }
+
+  editUserDetails() {
+
   }
 }
